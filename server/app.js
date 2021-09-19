7e112300026ae5bb22dbded86d745068bfd465eb
johadi <jim.hadi10@gmail.com>
2021-09-19 17:04:39 +0100

    Add route file

---
+++ server/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const fs = require('fs');
+
+const indexRouter = require('./routes');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.use('/', indexRouter);
+
+module.exports = app;
